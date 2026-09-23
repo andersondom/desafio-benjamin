1e05d69874d9ea8b6dff09406e05c21272368bce
Anderson Domingos <anderson@mica12.com.br>
2026-09-23 17:23:37 -0300

    Make home page load available subjects and real stats

--- DesafioBenjamin/Controllers/HomeController.cs
+++ DesafioBenjamin/Controllers/HomeController.cs
@@ -1,24 +1,42 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+using DesafioBenjamin.Data;
 using DesafioBenjamin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DesafioBenjamin.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
     {
-        return View();
+        _context = context;
     }
 
-    public IActionResult Privacy()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var questionarios = await _context.Questionarios
+            .Include(q => q.Disciplina)
+            .Where(q => q.Ativo && q.Disciplina.Ativa)
+            .OrderBy(q => q.Disciplina.Nome)
+            .ToListAsync();
+
+        ViewBag.TotalDesafios = await _context.Tentativas.CountAsync(t => t.FinalizadaEm != null);
+        ViewBag.TotalAcertos = await _context.Tentativas.SumAsync(t => (int?)t.TotalAcertos) ?? 0;
+        var totalQuestoes = await _context.Tentativas
+            .Where(t => t.FinalizadaEm != null)
+            .SumAsync(t => (int?)t.TotalQuestoes) ?? 0;
+        ViewBag.Aproveitamento = totalQuestoes == 0 ? 0 :
+            Math.Round(ViewBag.TotalAcertos * 100.0 / totalQuestoes, 1);
+
+        return View(questionarios);
     }
+
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Error() =>
+        View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
